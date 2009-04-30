4ec694c21bc231f74a9e8729c6b09fedbe7cfbf1
tclarke <tclarke@2d91f92e-8166-0410-902e-d52292f61e86>
2009-04-30 20:34:03 +0000

    Setup dependencies w/ multiple version builds.
    git-svn-id: https://opticks-extras.svn.sourceforge.net/svnroot/opticks-extras/branches/future/EXTRAS-1/IDL@27 2d91f92e-8166-0410-902e-d52292f61e86

--- Code/CompileSettings/idl.py
+++ Code/CompileSettings/idl.py
@@ -7,12 +7,12 @@ class IdlNotFound(SCons.Warnings.Warning):
 SCons.Warnings.enableWarningClass(IdlNotFound)
 
 def generate(env):
-    idlpath = os.environ.get('IDL_DIR',None)
+    idlpath = os.environ.get('OPTICKSDEPENDENCIES',None)
     if not idlpath:
        SCons.Warnings.warn(IdlNotFound,"Could not detect IDL")
     else:
-       env.AppendUnique(CXXFLAGS=["-I%s/external/include" % idlpath],
-                        LIBPATH=['%s/bin/bin.solaris2.sparc64' % idlpath],
+       env.AppendUnique(CXXFLAGS=["-I%s/Idl/include/6.4" % idlpath],
+                        LIBPATH=['%s/bin/bin.solaris2.sparc64/6.4' % idlpath],
                         LIBS=["idl","curses","sunmath"])
 
 def exists(env):
