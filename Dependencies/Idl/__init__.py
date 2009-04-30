70056f2d1c5a475b8a1fdd8b3af8c9a85c2c18b7
tclarke <tclarke@2d91f92e-8166-0410-902e-d52292f61e86>
2009-04-30 20:34:03 +0000

    Setup dependencies w/ multiple version builds.
    git-svn-id: https://extras.ballforge.net/svn/extras/branches/future/EXTRAS-1/IDL@27 2d91f92e-8166-0410-902e-d52292f61e86

---
+++ Dependencies/Idl/__init__.py
@@ -0,0 +1,4 @@
+__doc__ = """ITT IDL Libraries."""
+package = {'url':"https://opticks.ballforge.net/files/documents/8/278/Idl-23Apr2009.zip",
+           'downloadLocation':'Idl-23Apr2009.zip',
+           'targetDirectory':'Idl'}
