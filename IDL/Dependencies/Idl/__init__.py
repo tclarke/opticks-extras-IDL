c781023954ab7c7829e6453354a72d7cfb32827d
tclarke <tclarke@2d91f92e-8166-0410-902e-d52292f61e86>
2009-05-13 21:06:10 +0000

    Broken initial merge.
    git-svn-id: https://opticks-extras.svn.sourceforge.net/svnroot/opticks-extras/trunk/future@55 2d91f92e-8166-0410-902e-d52292f61e86

--- IDL/Dependencies/Idl/__init__.py
+++
@@ -1,4 +0,0 @@
-__doc__ = """ITT IDL Libraries."""
-package = {'url':"https://opticks.ballforge.net/files/documents/8/278/Idl-23Apr2009.zip",
-           'downloadLocation':'Idl-23Apr2009.zip',
-           'targetDirectory':'Idl'}
