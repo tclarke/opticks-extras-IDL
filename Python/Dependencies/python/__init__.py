ce46c1a5ecd6b175a2dadc9763548aa6c6d27b5e
tclarke <tclarke@2d91f92e-8166-0410-902e-d52292f61e86>
2009-10-05 16:02:20 +0000

    EXTRAS-4:Add a Python scripting engine.
    git-svn-id: https://opticks-extras.svn.sourceforge.net/svnroot/opticks-extras/trunk/1.0.X@124 2d91f92e-8166-0410-902e-d52292f61e86

---
+++ Python/Dependencies/python/__init__.py
@@ -0,0 +1,4 @@
+__doc__ = """Python Libraries."""
+package = {'url':"https://opticks.ballforge.net/files/documents/8/328/Python-2Sep09.zip",
+           'downloadLocation':'Python-2Sep09.zip',
+           'targetDirectory':'python'}
