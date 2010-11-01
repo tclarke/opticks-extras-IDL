b0088b04b21992356b5c315173be1871a4e3bed8
kstreith <kstreith@2d91f92e-8166-0410-902e-d52292f61e86>
2010-11-01 14:51:59 +0000

    EXTRAS-121:Update dependency urls to no longer use outdated opticks.ballforge.net server
    git-svn-id: svn://svn.code.sf.net/p/opticks-extras/code/IDL/trunk/future@1582 2d91f92e-8166-0410-902e-d52292f61e86

--- Dependencies/Idl/__init__.py
+++ Dependencies/Idl/__init__.py
@@ -1,4 +1,4 @@
 __doc__ = """ITT IDL Libraries."""
-package = {'url':"https://opticks.ballforge.net/files/documents/8/366/Idl-11Jan2010.zip",
+package = {'url':"http://opticks.org/downloads/dependencies/Idl-11Jan2010.zip",
            'downloadLocation':'Idl-11Jan2010.zip',
            'targetDirectory':'Idl'}
