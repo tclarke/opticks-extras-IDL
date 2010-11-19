5766a16fa3ce44dda34599ead6e658d6da44b121
kstreith <kstreith@2d91f92e-8166-0410-902e-d52292f61e86>
2010-11-19 12:56:47 +0000

    Updated version # to 1.2.0Unofficial since 1.2.0rc2 rollup is complete.
    git-svn-id: https://opticks-extras.svn.sourceforge.net/svnroot/opticks-extras/IDL/trunk/1.2.0@1725 2d91f92e-8166-0410-902e-d52292f61e86

--- Code/Include/IdlVersion.h
+++ Code/Include/IdlVersion.h
@@ -13,7 +13,7 @@
 #define IDL_NAME "IDL"
 #define IDL_NAME_LONG "IDL interpreter extension."
 #define IDL_COPYRIGHT "Copyright © 2009, Ball Aerospace & Technologies Corp."
-#define IDL_VERSION_NUMBER "1.2.0rc2"
+#define IDL_VERSION_NUMBER "1.2.0Unofficial"
 #define IDL_IS_PRODUCTION_RELEASE false
 
 #endif
